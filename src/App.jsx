7521035f6b21d98f485535fc1bd63f0af637bf82
Mariia <masha.shmidt.04@gmail.com>
2025-07-26 14:29:54 -0400

    theme change practice

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import ToDoList from './ToDoList'
 import Toggle from './Toggle'
 import ColorPicker from './ColorPicker'
 import Timer from './Timer'
+import Theme from './Theme'
 
 function App() {
 
   return (
     <>
+    <Theme/>
       <ToDoList/>
       <Toggle/>
       <ColorPicker/>
---
+++ src/Theme.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+function Theme(){
+ const [theme, setTheme]= useState('light');
+
+    function changeTheme(){
+        setTheme(theme === 'light' ? 'dark' : 'light');
+        console.log("Theme changed to:", theme === 'light' ? 'dark' : 'light');
+        
+    }
+
+    return (
+        <div style={{
+  backgroundColor: theme === "dark" ? "black" : "white",
+  color: theme === "dark" ? "white" : "black"
+}}className="theme-component">
+    <h1 style={{
+  backgroundColor: theme === "dark" ? "black" : "white",
+  color: theme === "dark" ? "white" : "black"
+}}>Theme Component</h1>
+        <button className="themeButton" onClick={changeTheme}>Change Theme</button>
+        <p className="themeText">Current theme: {theme === 'light' ? 'light' : 'dark'}</p>
+
+        </div>
+    )
+}
+export default Theme
