7a2ad534d71d6e7cef9a4457deb4877b42618523
Mariia <masha.shmidt.04@gmail.com>
2025-07-26 14:59:20 -0400

    quote fetching button added

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -3,7 +3,7 @@ function Quote() {
     const [quote, setQuote] = useState('');
     const [author, setAuthor] = useState('');
 
-   useEffect(()=>{
+   const fetchQuote= ()=>{
     fetch('https://api.quotable.io/random?limit=1')
     .then(response=> response.json())
     .then(data =>{
@@ -13,13 +13,19 @@ function Quote() {
       setAuthor(data.author);
         console.log("Quote fetched:", data.content, "by", data.author);
     })
-   }, [])
+   }
+     useEffect(() => {
+    fetchQuote();
+  }, []);
 
     return (
         <div className="quote-component">
             <h1>Random Quote</h1>
             <p className="quote-text">{quote}</p>
             <p className="quote-author">- {author}</p>
+            <button className="fetch-quote-button" onClick={fetchQuote}>
+                Fetch New Quote
+            </button>
         </div>
     );
 }
