c7fa9fc0e4b7bc410b55cdc7af8a189cfd23aecd
Mariia <masha.shmidt.04@gmail.com>
2025-07-29 18:42:49 -0400

    improved modal window

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,24 +1,36 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 
 function Modal(){
 
-    const [isHidden, setIsHidden] = useState(true)
+    const [isHidden, setIsHidden] = useState(false)
+    const modalRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (modalRef.current && !modalRef.current.contains(event.target)) {
+                setIsHidden(true);
+            }
+    }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     function handleModal(){
-        if (isHidden){
-            setIsHidden(false)
-        }else{
-            setIsHidden(true)
-        }
+        setIsHidden(false)
         
 
     }
 
     return(
-        <div className='modalComponent' onClick={handleModal}>
+        <div className='modalComponent'ref={modalRef} onClick={handleModal}>
             <h1>Modal component</h1>
             <p className='modalOutline'>Click here to open more info.</p>
-            <p className="modalHidden"> {isHidden && "This is hidden text. Click outside of modal component to close it."}</p>
-            </div>
+            { !isHidden && (
+        <p className="modalHidden">
+          This is hidden text. Click outside of modal component to close it.
+        </p>)} </div>
     )
 }
 export default Modal;
